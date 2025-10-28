48d56d237e4dc55539a161fabcaf50af631382f7
Yahyagitt <yahyamohd232@gmail.com>
2025-10-29 02:28:46 +0530

    Adding AutoTextEffect

--- AutoTextEffectAnimation/js/index.js
+++ AutoTextEffectAnimation/js/index.js
@@ -1,26 +1,25 @@
 const container = document.querySelector('.container');
-    careers = ["Student", "WebDev" ,"Vibecoder", "Explorer"]
-let careerIndex = 0;
-let characIndex = 0;
+    about = ["Student", "WebDev", "VibeCoder", "Explorer"];
 
+
+let abtIndex = 0;
+let charIndex = 0;
 
 function changeText() {
-    characIndex++;
+    charIndex++;
     container.innerHTML = `
-    <h1>
-    I am ${careers[careerIndex].slice(0,1) === "E" ? "an" : "a"} ${careers[careerIndex].slice(0,characIndex)}
-    </h1>
-    `;
-    
-    if(characIndex === careers[careerIndex].length){
-        careerIndex++;
-        characIndex = 0;
+    <h2>I am ${about[abtIndex].slice(0,1) === "E" ? "an":"a"} ${about[abtIndex].slice(0,charIndex)}</h2>
+    `
+    if(charIndex === about[abtIndex].length){
+        abtIndex++;
+        charIndex = 0;
     }
 
-    setTimeout(changeText,500);
-    if(careerIndex === careers.length){
-        careerIndex = 0;
+    if(abtIndex === about.length){
+        charIndex = 0;
+        abtIndex = 0;
     }
+    setTimeout(changeText, 500);
 }
 
 changeText();
