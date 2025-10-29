4a4050561d6e42841586e5c56a8c9dac3851005a
Yahyagitt <yahyamohd232@gmail.com>
2025-10-29 23:53:51 +0530

    Added Dark Mode Toggle project

--- DarkModeToggle/js/index.js
+++ DarkModeToggle/js/index.js
@@ -1,24 +1,29 @@
 const inputEl = document.querySelector('.input'),
-    body = document.querySelector('body')
+    body = document.querySelector('body');
 
-inputEl.addEventListener("input",() => {
-    changeBg();
+inputEl.addEventListener("input", () => {
+    changeBG();
     saveBG();
 })
 
-inputEl.checked = JSON.parse(localStorage.getItem("mode"));
-changeBg();
+inputEl.checked = JSON.parse(localStorage.getItem("mode")) || false;
+changeBG();
 
-function changeBg() {
+function changeBG() {
     if(inputEl.checked){
-        body.style.backgroundImage = "url('../imgs/toggleOff.svg')";
+        body.style.backgroundImage = "url('../imgs/toggleOn.svg')";
+        body.style.backgroundSize = "cover";
+        body.style.backgroundPosition = "center";
 
     } else {
-        body.style.backgroundImage = "url('../imgs/toggleOn.svg')";
+        body.style.backgroundImage = "url('../imgs/toggleOff.svg')";
+        body.style.backgroundSize = "cover";
+        body.style.backgroundPosition = "center";
+
     }
 }
 
-function saveBG(){
+function saveBG() {
     localStorage.setItem("mode",
         JSON.stringify(inputEl.checked)
     );
